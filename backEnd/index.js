1ac873cedec093e56dba4a1ac0f1519037806409
herilion <heritierlionge@yahoo.com>
2022-11-07 16:16:07 +0200

    update server

--- backEnd/index.js
+++ backEnd/index.js
@@ -1,6 +1,21 @@
 const express = require('express')
 const app = express();
 
-app.listen(8001, () => {
-    console.log('server est en marche');
+const cors = require('cors');
+
+app.use(express.urlencoded({ extends: true }));
+app.use(express.json());
+app.use(cors());
+
+const server = require('http').createServer(app);
+const PORT = 3001;
+const io = require('socket.io')(server, {
+    cors: {
+        origin: 'http:\\localhost:3000',
+        methods: ['GET', 'POST']
+    }
+})
+
+server.listen(PORT, () => {
+    console.log('server est en marche au', PORT);
 })
