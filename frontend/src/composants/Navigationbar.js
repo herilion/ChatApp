5dba98bbeb04181867e2ea5af17fb56b94c5430e
herilion <heritierlionge@yahoo.com>
2022-11-08 15:11:57 +0200

    update navbar

--- frontend/src/composants/Navigationbar.js
+++ frontend/src/composants/Navigationbar.js
@@ -1,16 +1,30 @@
 import React from 'react'
 import { Nav, Navbar, Container, NavDropdown } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import '../styles/Navigationbar.css'
+import solola from '../assets/solola.png'
 
 const Navigationbar = () => {
     return (
         <Navbar bg="light" expand="lg">
             <Container>
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <LinkContainer to='/home'>
+                    <Navbar.Brand>
+                        <div className='logoGroup'>
+                            <img src={solola} alt="logo solola app" className='ImgLogo' />
+                            <h2>Solola Chat</h2>
+                        </div>
+                    </Navbar.Brand>
+                </LinkContainer>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
+                    <Nav className="ms-auto">
+                        <LinkContainer to='/login'>
+                            <Nav.Link>Login</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to='/chat'>
+                            <Nav.Link>Chat</Nav.Link>
+                        </LinkContainer>
                         <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                             <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                             <NavDropdown.Item href="#action/3.2">
@@ -25,7 +39,7 @@ const Navigationbar = () => {
                     </Nav>
                 </Navbar.Collapse>
             </Container>
-        </Navbar>
+        </Navbar >
     )
 }
 export default Navigationbar;
