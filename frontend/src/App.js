f24049e959ef808eed9afed52ab700aee5c3301d
herilion <heritierlionge@yahoo.com>
2022-11-08 12:18:19 +0200

    configuration routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 import NavigationBar from './composants/Navigationbar';
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from './pages/Home';
+import SignUp from './pages/SignUp';
+import Login from './pages/Login';
+import Chat from './pages/Chat';
 function App() {
   return (
     <BrowserRouter>
       <NavigationBar />
       <Routes>
-        <Route path='element=' />
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/chat' element={<Chat />} />
       </Routes>
     </BrowserRouter>
   );
