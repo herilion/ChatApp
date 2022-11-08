dcc0ea6c90e5c2dd701ea51639d978624950f909
herilion <heritierlionge@yahoo.com>
2022-11-08 16:54:49 +0200

    update home page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { Row, Col, Button } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap';
+import '../styles/Home.css'
 
 const Home = () => {
     return (
         <Row>
-            <Col md={6}>
-                <h1>Partagez le mot avec vos amis</h1>
-                <p>Communiquez aussi facilement que rapidement</p>
-                <LinkContainer to='/chat'>
-                    <Button variant='success'>Commencer</Button>
-                </LinkContainer>
+            <Col md={6} className='d-flex flex-direction-column align-items-center justify-content-center'>
+                <div>
+                    <h1>Partagez le mot avec vos amis</h1>
+                    <p>Communiquez aussi facilement que rapidement</p>
+                    <LinkContainer to='/chat'>
+                        <Button variant='success'>Commencer<i className='fas fa-comments home-message-icon'></i></Button>
+
+                    </LinkContainer>
+                </div>
             </Col>
+            <Col md={6} className='bgHome'></Col>
         </Row>
     )
 }
