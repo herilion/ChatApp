e36ba66f93d502f2b7b42d160317231674cf6e05
herilion <heritierlionge@yahoo.com>
2022-11-08 16:18:14 +0200

    update home page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { Row, Col, Button } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap';
 
 const Home = () => {
     return (
-        <div>Home bhfdvdvdvyy</div>
+        <Row>
+            <Col md={6}>
+                <h1>Partagez le mot avec vos amis</h1>
+                <p>Communiquez aussi facilement que rapidement</p>
+                <LinkContainer to='/chat'>
+                    <Button variant='success'>Commencer</Button>
+                </LinkContainer>
+            </Col>
+        </Row>
     )
 }
 
