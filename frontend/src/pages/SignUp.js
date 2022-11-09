862ee01abe1e2ff98dc565dc509945a4e68ce5df
herilion <heritierlionge@yahoo.com>
2022-11-09 09:48:59 +0200

    update login form and signUp form

--- frontend/src/pages/SignUp.js
+++ frontend/src/pages/SignUp.js
@@ -10,11 +10,11 @@ const SignUp = () => {
                     <Form className='form'>
                         <Form.Group className="mb-3" controlId="formBasicPassword">
                             <Form.Label>Votre Nom</Form.Label>
-                            <Form.Control type="text" placeholder="Votre Nom ici" />
+                            <Form.Control type="text" placeholder="Votre Nom ici" required />
                         </Form.Group>
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                             <Form.Label>Email address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" />
+                            <Form.Control type="email" placeholder="Enter email" required />
                             <Form.Text className="text-muted">
                                 We'll never share your email with anyone else.
                             </Form.Text>
