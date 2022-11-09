50e933db1114006d8e4e6a2fe0698d73d7e055ce
herilion <heritierlionge@yahoo.com>
2022-11-09 11:17:42 +0200

    connection à la base des données

--- backEnd/index.js
+++ backEnd/index.js
@@ -1,21 +1,33 @@
 const express = require('express')
 const app = express();
-
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const cors = require('cors');
 
-app.use(express.urlencoded({ extends: true }));
+dotenv.config();
+
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 
-// const server = require('http').createServer(app);
-const PORT = 3001;
+const DB_URL = process.env.DATABASE_URL;
+
+const server = require('http').createServer(app);
+const PORT = process.env.PORT;
 const io = require('socket.io')(server, {
     cors: {
         origin: 'http:\\localhost:3000',
         methods: ['GET', 'POST']
     }
 })
+console.log(DB_URL)
+mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log('server est en marche au', PORT);
+        })
+    })
+    .catch(err => {
+        console.log(err.message);
+    })
 
-app.listen(PORT, () => {
-    console.log('server est en marche au', PORT);
-})
