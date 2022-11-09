9d34c5e67b603a192e1ab4f223b1035808a47112
herilion <heritierlionge@yahoo.com>
2022-11-09 09:31:58 +0200

    update signUp

--- frontend/src/pages/SignUp.js
+++ frontend/src/pages/SignUp.js
@@ -8,6 +8,10 @@ const SignUp = () => {
                 <Col md={5} className='bgLogin'></Col>
                 <Col md={5} className='d-flex align-items-center justify-content-center flex-direction-column'>
                     <Form className='form'>
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Votre Nom</Form.Label>
+                            <Form.Control type="text" placeholder="Votre Nom ici" />
+                        </Form.Group>
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                             <Form.Label>Email address</Form.Label>
                             <Form.Control type="email" placeholder="Enter email" />
@@ -18,6 +22,10 @@ const SignUp = () => {
 
                         <Form.Group className="mb-3" controlId="formBasicPassword">
                             <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password" />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Confirmer votre mot de pass</Form.Label>
                             <Form.Control type="password" placeholder="Password" />
                         </Form.Group>
                         <Button variant="primary" type="submit">
