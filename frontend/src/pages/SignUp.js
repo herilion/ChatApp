106d55e6faf49381d920e0da43a4e0d1ffb4abaf
herilion <heritierlionge@yahoo.com>
2022-11-09 09:23:17 +0200

    add signUp

--- frontend/src/pages/SignUp.js
+++ frontend/src/pages/SignUp.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from 'react';
+import { Row, Col, Form, Button, Container } from 'react-bootstrap';
 
 const SignUp = () => {
     return (
-        <div>SignUp</div>
-    )
+        <Container>
+            <Row>
+                <Col md={5} className='bgLogin'></Col>
+                <Col md={5} className='d-flex align-items-center justify-content-center flex-direction-column'>
+                    <Form className='form'>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" />
+                            <Form.Text className="text-muted">
+                                We'll never share your email with anyone else.
+                            </Form.Text>
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password" />
+                        </Form.Group>
+                        <Button variant="primary" type="submit">
+                            Créer un compte
+                        </Button>
+                    </Form>
+                </Col>
+            </Row>
+        </Container>
+    );
+
 }
 
 export default SignUp
