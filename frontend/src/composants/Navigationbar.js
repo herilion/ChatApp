3a73499bf87aa8fcb72430bd5e2df48f610cc808
herilion <heritierlionge@yahoo.com>
2022-11-10 11:46:27 +0200

    update navBar

--- frontend/src/composants/Navigationbar.js
+++ frontend/src/composants/Navigationbar.js
@@ -25,15 +25,10 @@ const Navigationbar = () => {
                         <LinkContainer to='/chat'>
                             <Nav.Link>Chat</Nav.Link>
                         </LinkContainer>
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                        <NavDropdown title="Action" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="/login">Se deconnecter</NavDropdown.Item>
                             <NavDropdown.Item href="#action/3.2">
-                                Another action
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">
-                                Separated link
+                                Changer de compte
                             </NavDropdown.Item>
                         </NavDropdown>
                     </Nav>
