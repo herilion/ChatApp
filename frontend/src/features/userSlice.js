3eb5c9488f74df71491fbf926c0e5990a394ec7f
herilion <heritierlionge@yahoo.com>
2022-11-17 11:30:31 +0200

    to save user after signin

--- frontend/src/features/userSlice.js
+++ frontend/src/features/userSlice.js
@@ -11,6 +11,8 @@ export const userSlice = createSlice({
 
     extraReducers: (builder) => {
         //save user after signup
-        builder.addMatcher(appApi.endpoints.signupUser.matchFulfilled, (state, { payload }) => payload)
+        builder.addMatcher(appApi.endpoints.signupUser.matchFulfilled, (state, { payload }) => payload);
+        //save user after login
+        builder.addMatcher(appApi.endpoints.loginUser.matchFulfilled, (state, { payload }) => payload);
     }
 });
