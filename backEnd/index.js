aff7e8b491d6c975bccfbce3e7a610996803f920
herilion <heritierlionge@yahoo.com>
2022-11-24 11:43:59 +0200

    sort message by date

--- backEnd/index.js
+++ backEnd/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 const app = express();
 import userRoutes from './routes/userRoutes.js';
-import { connect, isObjectIdOrHexString } from 'mongoose';
+import { connect } from 'mongoose';
 import { config } from 'dotenv';
 import cors from 'cors';
 import User from './models/Users.js'
@@ -29,9 +29,7 @@ const io = new Server(server, {
         methods: ['GET', 'POST']
     }
 })
-app.get('/rooms', (req, res) => {
-    res.json(rooms)
-})
+
 
 //get last message from room
 const getLastMessagesFromRoom = async (room) => {
@@ -43,7 +41,7 @@ const getLastMessagesFromRoom = async (room) => {
 }
 
 // sort room message by date
-const sortRoomMessagesByDate = async (messages) => {
+const sortRoomMessagesByDate = (messages) => {
     return messages.sort(function (x, y) {
         let date1 = x._id.split('/');
         let date2 = y._id.split('/');
@@ -67,14 +65,16 @@ io.on('connection', (socket) => {
     })
 
     //joint room
-    socket.on('join-room', async (room) => {
-        socket.join(room);
-        let roomMessages = await getLastMessagesFromRoom(room);
+    socket.on('join-room', async (newRoom, previousRoom) => {
+        socket.join(newRoom);
+        socket.leave(previousRoom);
+        let roomMessages = await getLastMessagesFromRoom(newRoom);
         roomMessages = sortRoomMessagesByDate(roomMessages);
         socket.emit('room-messages', roomMessages)
     })
 
     socket.on('message-room', async (room, content, sender, time, date) => {
+        // console.log('new message', content);
         const newMessage = await Message.create({ content, from: sender, time, date, to: room });
         let roomMessages = await getLastMessagesFromRoom(room);
         roomMessages = sortRoomMessagesByDate(roomMessages);
@@ -83,6 +83,29 @@ io.on('connection', (socket) => {
         //add notification
         socket.broadcast.emit('notifications', room)
     })
+
+    //logout user
+
+    app.delete('/logout', async (req, res) => {
+        try {
+            const { _id, newMessages } = req.body;
+            const user = await User.findById(_id);
+            user.status = "offline";
+            user.newMessages = newMessages;
+            await user.save();
+            const members = await User.find();
+            socket.broadcast.emit('new-user', members);
+            res.status(200).send();
+        } catch (e) {
+            console.log(e);
+            res.status(400).send()
+        }
+    })
+
+
+})
+app.get('/rooms', (req, res) => {
+    res.json(rooms)
 })
 
 console.log(DB_URL)
