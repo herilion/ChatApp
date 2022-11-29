dbbaf1e0d20ba140a4747db1358ad5a3d2bfb512
herilion <heritierlionge@yahoo.com>
2022-11-29 15:50:12 +0200

    update style login form

--- backEnd/index.js
+++ backEnd/index.js
@@ -1,125 +1,51 @@
-import express from 'express';
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/auth");
+const messageRoutes = require("./routes/messages");
 const app = express();
-import userRoutes from './routes/userRoutes.js';
-import { connect } from 'mongoose';
-import { config } from 'dotenv';
-import cors from 'cors';
-import User from './models/Users.js'
-import Message from './models/Message.js';
-const rooms = ['Room 1', 'Room 2', 'Room3', 'Room4'];
+const socket = require("socket.io");
+require("dotenv").config();
 
-import { Server } from 'socket.io';
-
-
-config();
-
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 app.use(cors());
-app.use('/users', userRoutes)
+app.use(express.json());
 
-const DB_URL = process.env.DATABASE_URL;
-import http, { get } from 'http';
-const server = http.createServer(app);
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connetion Successfull");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 
-const PORT = process.env.PORT;
-const io = new Server(server, {
-    cors: {
-        origin: 'http://localhost:3000',
-        methods: ['GET', 'POST']
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+const server = app.listen(process.env.PORT, () =>
+  console.log(`Server started on ${process.env.PORT}`)
+);
+const io = socket(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    credentials: true,
+  },
+});
+
+global.onlineUsers = new Map();
+io.on("connection", (socket) => {
+  global.chatSocket = socket;
+  socket.on("add-user", (userId) => {
+    onlineUsers.set(userId, socket.id);
+  });
+
+  socket.on("send-msg", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-recieve", data.msg);
     }
-})
-
-
-//get last message from room
-const getLastMessagesFromRoom = async (room) => {
-    let roomMessages = await Message.aggregate([
-        { $match: { to: room } },
-        { $group: { _id: '$date', messagesByDate: { $push: '$$ROOT' } } }
-    ])
-    return roomMessages;
-}
-
-// sort room message by date
-const sortRoomMessagesByDate = (messages) => {
-    return messages.sort(function (x, y) {
-        let date1 = x._id.split('/');
-        let date2 = y._id.split('/');
-
-        date1 = date1[2] + date1[0] + date1[1]
-        date2 = date2[2] + date2[0] + date2[1];
-
-        return date1 < date2 ? -1 : 1
-    })
-}
-
-// socket connexion
-
-io.on('connection', (socket) => {
-    console.log('un clien vient de se connecter');
-
-    // contact new user
-    socket.on('new-user', async () => {
-        const members = await User.find();
-        io.emit('new-user', members)
-    })
-
-    //joint room
-    socket.on('join-room', async (newRoom, previousRoom) => {
-        socket.join(newRoom);
-        socket.leave(previousRoom);
-        let roomMessages = await getLastMessagesFromRoom(newRoom);
-        roomMessages = sortRoomMessagesByDate(roomMessages);
-        socket.emit('room-messages', roomMessages)
-    })
-
-    socket.on('message-room', async (room, content, sender, time, date) => {
-        console.log('new message', content);
-        const newMessage = await Message.create({ content, from: sender, time, date, to: room });
-        // console.log(newMessage);
-        // socket.emit('newMessage', newMessages)
-        let roomMessages = await getLastMessagesFromRoom(room);
-        roomMessages = sortRoomMessagesByDate(roomMessages);
-        //sending message to room
-        socket.to(room).emit('room-messages', roomMessages);
-        // io.to(room).emit('room-messages', roomMessages);
-        //add notification
-        socket.broadcast.emit('notifications', room)
-    })
-
-    //logout user
-
-    app.delete('/logout', async (req, res) => {
-        try {
-            const { _id, newMessages } = req.body;
-            const user = await User.findById(_id);
-            user.status = "offline";
-            user.newMessages = newMessages;
-            await user.save();
-            const members = await User.find();
-            socket.broadcast.emit('new-user', members);
-            res.status(200).send();
-        } catch (e) {
-            console.log(e);
-            res.status(400).send()
-        }
-    })
-
-
-})
-app.get('/rooms', (req, res) => {
-    res.json(rooms)
-})
-
-console.log(DB_URL)
-//connection à la base des données
-connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-        server.listen(PORT, () => {
-            console.log('server est en marche au', PORT);
-        })
-    })
-    .catch(err => {
-        console.log(err.message);
-    })
-
+  });
+});
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,48 +1,18 @@
-import './App.css';
-import NavigationBar from './composants/Navigationbar';
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Home from './pages/Home';
-import SignUp from './pages/SignUp';
-import Login from './pages/Login';
-import Chat from './pages/Chat';
-import { useSelector } from 'react-redux';
-import { useState } from 'react';
-import { AppContext, socket } from './contexts/appContext';
-
-function App() {
-  //the rooms
-  const [rooms, setRooms] = useState([]);
-  const [currentRoom, setCurrentRoom] = useState([]);
-  // users and message
-  const [members, setMembers] = useState([]);
-  const [messages, setMessages] = useState([]);
-  const [privateMemberMsg, setPrivateMemberMsg] = useState({});
-  const [newMessages, setNewMessages] = useState({});
-  const user = useSelector((state) => state.user);
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SetAvatar from "./components/SetAvatar";
+import Chat from "./pages/Chat";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+export default function App() {
   return (
-    <AppContext.Provider value={{
-      socket, currentRoom, setCurrentRoom,
-      members, setMembers,
-      messages, setMessages,
-      privateMemberMsg, setPrivateMemberMsg,
-      rooms, setRooms,
-      newMessages, setNewMessages
-    }}>
-      <BrowserRouter>
-        <NavigationBar />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          {!user && (
-            <>
-              <Route path='/home' element={<Home />} />
-              <Route path='/login' element={<Login />} />
-            </>
-          )}
-          <Route path='/signup' element={<SignUp />} />
-          <Route path='/chat' element={<Chat />} />
-        </Routes>
-      </BrowserRouter>
-    </AppContext.Provider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/setAvatar" element={<SetAvatar />} />
+        <Route path="/" element={<Chat />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-export default App;
