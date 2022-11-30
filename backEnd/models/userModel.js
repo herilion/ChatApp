a5c25de26b7d30d37aa966b51d2c1168f3e11e72
herilion <heritierlionge@yahoo.com>
2022-11-30 21:15:22 +0200

    user schema

--- backEnd/models/userModel.js
+++ backEnd/models/userModel.js
@@ -5,19 +5,17 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
     min: 3,
-    max: 20,
     unique: true,
   },
   email: {
     type: String,
     required: true,
     unique: true,
-    max: 50,
   },
   password: {
     type: String,
     required: true,
-    min: 8,
+    min: 6,
   },
   isAvatarImageSet: {
     type: Boolean,
